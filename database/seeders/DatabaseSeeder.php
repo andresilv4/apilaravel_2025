3a75e8e861c6cab7018a143727e0b92a5dd126c9
andresilv4 <andreffranco16@gmail.com>
2025-09-30 11:07:51 -0300

    nova atualizacao

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tarefa;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,46 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        //User::factory()->create([
+            //'name' => 'Test User',
+            //'email' => 'test@example.com',
+        //]);
+
+        Tarefa::insert([
+            [
+                "texto" => "Preparar o café da manhã",
+            ],
+            [
+                "texto" => "Levar as crianças para a escola (se aplicável)",
+            ],
+            [
+                "texto" => "Verificar e-mails importantes",
+            ],
+            [
+                "texto" => "Participar da reunião de equipe das 10h",
+            ],
+            [
+                "texto" => "Almoçar e fazer uma pequena pausa",
+            ],
+            [
+                "texto" => "Revisar o relatório do projeto X",
+            ],
+            [
+                "texto" => "Fazer uma caminhada de 30 minutos",
+            ],
+            [
+                "texto" => "Responder às pendências da tarde",
+            ],
+            [
+                "texto" => "Preparar o jantar",
+            ],
+            [
+                "texto" => "Ler 1 capítulo de um livro",
+            ],
+            [
+                "texto" => "Organizar a área de trabalho para o dia seguinte",
+            ],
         ]);
+        
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Models\Tarefa;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/somar/{x}/{y}', function($x, $y) {
+    return $x + $y;
+});
+
+Route::get('/info', function(){
+    $dados = [
+        'nome' => 'Eusébio',
+        'idade' => '18',
+        'cidade' => 'Bento Gonçalves',
+        'fruta favorita' => 'amora'
+    ];
+
+    return response()->json($dados, 200);
+});
+
+Route::get('/tarefas', function() {
+    //return Tarefa::all();
+    return Tarefa::imaginate(5);
 });
